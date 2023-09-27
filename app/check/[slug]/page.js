58607cb06c9cb59684238fcc52b083e5504b6beb
mjunaid9590 <mjunaid9590@gmail.com>
2023-09-27 08:18:06 +0500

    implemented static params

--- app/check/[slug]/page.js
+++ app/check/[slug]/page.js
@@ -1,4 +1,16 @@
 import React from 'react'
+export async function generateStaticParams() {
+  
+ 
+  return [
+    {
+      slug: 'hicken'
+    },
+    {
+      slug: 'meat'
+    }
+  ]
+}
 
 const page = ({params}) => {
     const {slug}=params
